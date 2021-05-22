d566348425d616a7661b0d3d1c5a02d4d9882ec6
dougrchamberlain <d.chamberlain@nextech.com>
2021-05-22 16:31:21 -0400

    3 files modified

--- src/app/medications.service.ts
+++ src/app/medications.service.ts
@@ -1,13 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ObservableStore } from '@codewithdan/observable-store';
 import { Observable } from 'rxjs';
-import { API_URL } from '../environments/environment';
+import { API_URL, environment } from '../environments/environment';
+import * as jmsepath from 'jmsepath';
 
-@Injectable()
-export class MedicationsService {
-  constructor(private client: HttpClient) {}
-
-  get(): Observable<any> {
-    return this.client.get<any>(API_URL);
+@Injectable({ providedIn: 'root' })
+export class MedicationsService extends ObservableStore<StoreState> {
+  apiPath: string = API_URL;
+  constructor(private client: HttpClient) {
+    super({ trackStateHistory: true });
+    const initialState: StoreState = {
+      documents: [],
+      document: { url: '', description: '', subtype: '', type: '' }
+    };
+    this.setState(initialState, 'INIT_STATE');
   }
+
+  get(id: any): Observable<Medication[]> {
+    const o$ = this.client.post<any>(
+      this.apiPath,
+      {},
+      { params: { patientId: id } }
+    );
+
+    o$.subscribe({
+      next: (data: any) => {
+        const documents = jmsepath.search(
+          data.PracticeFileTypeModels,
+          environment.jmesPathSearchString
+        );
+        this.setState({ documents }, StoreActions.GetDocuments);
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+
+    return o$;
+  }
+
+  select(doc: Medication) {
+    this.setState({ document: doc });
+  }
+}
+export interface StoreState {
+  documents: Medication[];
+  document: Medication | null;
 }
